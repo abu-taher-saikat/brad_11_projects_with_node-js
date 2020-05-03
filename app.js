76631d36109945b1fb3a980f03d2e78dc737d5cb
Abu Taher Saikat <38492131+saikat1095@users.noreply.github.com>
2020-05-04 01:16:29 +0600

    sleepy

--- app.js
+++ app.js
@@ -2,8 +2,32 @@ const express = require('express')
 const app = express();
 
 
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({
+    extended: false
+}))
+
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', {
+        name: "saikat"
+    })
 })
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+app.post('/login', (req, res) => {
+    res.render('')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+    // res.render('register.ejs');
+})
+
+
 app.listen(3000)
